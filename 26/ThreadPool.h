9aac9d58f5f1c2c8072d5cb7142fd66667c3a16f
fayashi233 <jiangyang10@outlook.com>
2024-08-15 11:25:39 +0800

    实现多线程的主从Reactor模型

---
+++ 26/ThreadPool.h
@@ -0,0 +1,31 @@
+#include <queue>
+#include <vector>
+#include <mutex>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <thread>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <atomic>
+
+class ThreadPool
+{
+private:
+    std::vector<std::thread> threads_;            // 线程池中的线程
+    std::queue<std::function<void()>> taskqueue_; // 任务队列
+    std::mutex mutex_;                            // 任务队列同步互斥锁
+    std::condition_variable condition_;           // 任务队列同步的条件变量
+    std::atomic_bool stop_;                       // 在析构函数中，把stop_的值设置为false，全部的线程将退出
+public:
+    ThreadPool(size_t threadnum);
+
+    //在析构函数中停止线程
+    ~ThreadPool();
+
+    //把任务添加到线程队列中
+    void addtask(std::function<void()> task);
+
+
+};
+
